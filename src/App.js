f0bd68e312499fc48a35280e510aaaab7c907e70
Abraham-Ragab <Abrahammoustafa@hotmail.co.uk>
2019-01-29 17:58:21 +0200

    Add starter code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import Board from './Board'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div></div>
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
     );
   }
 }

a22a83db1ea9f2c163cd00488e89602ab8a9a425
Abraham-Ragab <Abrahammoustafa@hotmail.co.uk>
2019-01-29 19:43:30 +0200

    update squre component to Function Components

--- src/Square.js
+++ src/Square.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 
 
-class Square extends Component {
-
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
+function Square(props) {
+  return (
+    <button
+      className='square'
+      onClick={props.onClick}>
+        {props.value}
       </button>
-    );
-  }
+  );
 }
 
 export  default Square;

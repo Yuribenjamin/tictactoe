cf6e6fe08960f197c535e2aff4dc6cede2c3c5f4
Abraham-Ragab <Abrahammoustafa@hotmail.co.uk>
2019-02-05 19:22:21 +0200

    Updates

---
+++ precache-manifest.282dc18025aa5b2d36c72176176a94fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d808fef0da889f09742d3a074f356bc5",
+    "url": "/tictactoe/static/media/qbkls.d808fef0.png"
+  },
+  {
+    "revision": "a6a71d35c1fbb3241cd8",
+    "url": "/tictactoe/static/js/runtime~main.a6a71d35.js"
+  },
+  {
+    "revision": "184b3dc2e915a9980613",
+    "url": "/tictactoe/static/js/main.184b3dc2.chunk.js"
+  },
+  {
+    "revision": "abd9e857442f04b96f9b",
+    "url": "/tictactoe/static/js/1.abd9e857.chunk.js"
+  },
+  {
+    "revision": "184b3dc2e915a9980613",
+    "url": "/tictactoe/static/css/main.06fe4ecd.chunk.css"
+  },
+  {
+    "revision": "793cf6bd97dcbca42e7bb4b2aab7e6f6",
+    "url": "/tictactoe/index.html"
+  }
+];

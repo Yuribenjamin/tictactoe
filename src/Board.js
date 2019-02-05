430e7a3f99632e216351979b04caae7994e02c43
Abraham-Ragab <Abrahammoustafa@hotmail.co.uk>
2019-02-05 18:48:13 +0200

    Declare the winner

--- src/Board.js
+++ src/Board.js
@@ -5,16 +5,19 @@ class Board extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      Squares: Array(9).fill(null),
+      squares: Array(9).fill(null),
       xIsNext: true,
     }
   }
 
   handleClick(i) {
-    const Squares = this.state.Squares.slice();
-    Squares[i] = this.state.xIsNext ? 'X' : 'O';
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
-      Squares: Squares,
+      squares: squares,
       xIsNext: !this.state.xIsNext,
     });
   }
@@ -22,15 +25,20 @@ class Board extends Component {
   renderSquare(i) {
     return (
       <Square 
-        value={this.state.Squares[i]}
+        value={this.state.squares[i]}
         onClick={() => this.handleClick(i)}
       />
     );
   }
   
     render() {
-      const status = 'Next Player: ' +
-      (this.state.xIsNext ? 'X' : 'O');
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
   
       return (
         <div>
@@ -56,4 +64,23 @@ class Board extends Component {
 }
 
 export default Board;
-  
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
--- src/Square.js
+++ src/Square.js
@@ -11,4 +11,4 @@ function Square(props) {
   );
 }
 
-export  default Square;
+export default Square;

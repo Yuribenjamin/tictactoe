d00f68d765f0143db5946b05dbaa456b24f925de
Abraham-Ragab <Abrahammoustafa@hotmail.co.uk>
2019-02-05 19:06:28 +0200

    Display Past Moves

--- src/App.js
+++ src/App.js
@@ -34,6 +34,17 @@ class App extends Component {
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
 
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
@@ -51,7 +62,7 @@ class App extends Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
